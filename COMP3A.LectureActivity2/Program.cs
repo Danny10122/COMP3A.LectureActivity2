f9e792e30e0de66c1f8ce0bffab845d0ae21c0c4
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-21 20:16:18 -0800

    Added using Interpolation and adding comparions

--- COMP3A.LectureActivity2/Program.cs
+++ COMP3A.LectureActivity2/Program.cs
@@ -7,8 +7,32 @@
             Console.Write("Enter the first number:");
             string input1 = Console.ReadLine();
             Console.Write("Emter the second number:");
-            string input2 = Console.ReadLine();    
+            string input2 = Console.ReadLine();
+
+            //For the sum and avg
+            double num1 = double.Parse(input1);
+            double num2 = double.Parse(input2);
+
+            double sum = num1 + num2;
+            double average = sum / 2;
+            
+            //Using Concatenations table
             Console.WriteLine("\nUsing Concatenation");
+            Console.WriteLine("Sum: " +sum);
+            Console.WriteLine("Average:" + average);
+
+            //Using Interpolation
+            Console.WriteLine("\nUsing Interpolation");
+            Console.WriteLine($"Sum: {sum}" );
+            Console.WriteLine($"Average: {average}");
+
+            //Comparsions
+            Console.WriteLine("\nComparisons");
+            Console.WriteLine($"{num1} > {num2}");
+            Console.WriteLine();
+            
+
+
 
         }
     }
